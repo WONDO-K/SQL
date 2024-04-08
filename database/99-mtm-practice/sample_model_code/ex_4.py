e76ad7105426bfc25ed18379504bf01ee37a3b4b
wondo-k <wondok95@naver.com>
2024-04-08 17:35:40 +0900

    [SQL] 2024-04-08

---
+++ database/99-mtm-practice/sample_model_code/ex_4.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class Doctor(models.Model):
+    name = models.TextField()
+
+    def __str__(self):
+        return f'{self.pk}번 의사 {self.name}'
+
+
+class Patient(models.Model):
+    # ManyToManyField - related_name 작성
+    # related_name='patients' 역참조 매니저 이름 변경하기
+    doctors = models.ManyToManyField(Doctor, related_name='patients')
+    name = models.TextField()
+
+    def __str__(self):
+        return f'{self.pk}번 환자 {self.name}'
